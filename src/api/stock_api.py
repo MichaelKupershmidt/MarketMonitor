fd25745332abe01a51cffb1e3f4dfe1f9b97313a
Michael kupershmidt <michaelkupershmidt@Michaels-MacBook-Pro-9.local>
2024-05-28 21:15:58 -0400

    first table schema

--- src/api/stock_api.py
+++ src/api/stock_api.py
@@ -22,21 +22,21 @@ finnhub_client = finnhub.Client(api_key=finhub_api_key)
 symbol = 'AAPL'
 
 # Define the timeframe for which you want financial data (e.g., last 30 days)
-end_date = datetime.now()
-start_date = end_date - timedelta(days=30)
+# end_date = datetime.now()
+# start_date = end_date - timedelta(days=30)
 
-# Format dates as UNIX timestamps
-start_timestamp = int(start_date.timestamp())
-end_timestamp = int(end_date.timestamp())
+# # Format dates as UNIX timestamps
+# start_timestamp = int(start_date.timestamp())
+# end_timestamp = int(end_date.timestamp())
 
 # Fetch basic financials data using the FinHub client
-response = finnhub_client.company_basic_financials(symbol, 'all')
+response = finnhub_client.company_profile2(symbol='AAPL')
 
 # Ensure the 'raws' directory exists
 os.makedirs('raws', exist_ok=True)
 
 # Save the raw JSON data to a file in the 'raws' directory
-json_filename = os.path.join('raws', 'financial_data.json')
+json_filename = os.path.join('raws', 'profile_data.json')
 with open(json_filename, 'w') as json_file:
     json.dump(response, json_file, indent=4)
 
