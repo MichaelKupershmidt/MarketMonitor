a4816da7d9dcd718463529b5d144cb7e9997fe35
Michael kupershmidt <michaelkupershmidt@Michaels-MacBook-Pro-9.local>
2024-05-30 23:01:15 -0400

    define api get functions

--- src/api/stock_api.py
+++ src/api/stock_api.py
@@ -3,41 +3,28 @@ import pandas as pd
 import os
 from datetime import datetime, timedelta
 import json
-# Your FinHub API key
-finhub_api_key = os.environ.get('FINHUB_API_KEY')
-
 import finnhub
-import pandas as pd
-import os
-import time
+import requests
+from bs4 import BeautifulSoup
 
-import finnhub
-
+def get_sp500_tickers():
+    sp500_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    response = requests.get(sp500_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', {'id': 'constituents'})
+    
+    # Parse the table
+    df = pd.read_html(str(table))[0]
+    sp500_tickers = df['Symbol'].tolist()
+    
+    return sp500_tickers
 # Setup client
-finnhub_client = finnhub.Client(api_key=finhub_api_key)
+finnhub_client = finnhub.Client(api_key='c7f78v2ad3ib5ruekktg')
 
-# Basic financials
-#print(finnhub_client.company_basic_financials('AAPL', 'all'))
+def get_company_profile(ticker):
+    response = finnhub_client.company_profile2(symbol=ticker)
+    return response
 
-symbol = 'AAPL'
-
-# Define the timeframe for which you want financial data (e.g., last 30 days)
-# end_date = datetime.now()
-# start_date = end_date - timedelta(days=30)
-
-# # Format dates as UNIX timestamps
-# start_timestamp = int(start_date.timestamp())
-# end_timestamp = int(end_date.timestamp())
-
-# Fetch basic financials data using the FinHub client
-response = finnhub_client.company_profile2(symbol='AAPL')
-
-# Ensure the 'raws' directory exists
-os.makedirs('raws', exist_ok=True)
-
-# Save the raw JSON data to a file in the 'raws' directory
-json_filename = os.path.join('raws', 'profile_data.json')
-with open(json_filename, 'w') as json_file:
-    json.dump(response, json_file, indent=4)
-
-print(f"Financial data saved successfully as '{json_filename}'.")
+def get_company_metrics(ticker):
+    response = finnhub_client.company_basic_financials(ticker, 'all')
+    return response['metric']
